dd370f8e6a89a549f80157a8c214b3e2bf9d0f05
Netaly <25016486+21shield@users.noreply.github.com>
2024-05-27 19:18:22 -0400

    Updates styles for sliderbar

--- src/components/StepThree.js
+++ src/components/StepThree.js
@@ -1,6 +1,14 @@
+import "../styles/stepThree.css";
 import React from "react";
 
 const StepThree = ({ setStep, inputFields, setInputFields }) => {
+  const handleInputEvent = e => {
+    // this will give us access to a value 1-10
+    setInputFields(prev => ({
+      ...prev,
+      abstract: e.target.value,
+    }));
+  };
   return (
     <div className="fields_box">
       <div className="fields_box">
@@ -9,7 +17,21 @@ const StepThree = ({ setStep, inputFields, setInputFields }) => {
         </label>
         <span>Take your best guess, no need to stress it.</span>
 
-        <input type="range" id="abstract" name="abstract" min="0" max="10" />
+        <input
+          type="range"
+          id="abstract"
+          name="abstract"
+          className="abstract_meter"
+          min="0"
+          max="10"
+          style={{
+            background: `linear-gradient(to right, #0A0C0E ${
+              inputFields.abstract * 10
+            }%, #D9D9D9 ${inputFields.abstract * 10}%)`,
+          }}
+          value={inputFields.abstract}
+          onInput={handleInputEvent}
+        />
 
         <button className="main_btn" onClick={() => setStep(4)}>
           Next
