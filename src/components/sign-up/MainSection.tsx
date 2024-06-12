7cd5e620b441e69f626286d43e9b35228dc76471
Netaly Ramirez <25016486+21shield@users.noreply.github.com>
2024-06-12 18:26:41 -0400

    Update to typescript (#8)

--- src/components/sign-up/MainSection.tsx
+++ src/components/sign-up/MainSection.tsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from "react";
 import SignUpForm from "./SignUpForm";
-import ProgressBar from "./ProgressBar.tsx";
-import { InitialScreen } from "./InitialScreen.tsx";
-
+import ProgressBar from "./ProgressBar";
+import { InitialScreen } from "./InitialScreen";
 
 const MainSection = ({ joined, setJoined }) => {
   const [step, setStep] = useState(1);
   return (
     <section className="main_section">
       {joined === false ? (
-        <InitialScreen setJoined={setJoined}/>
+        <InitialScreen setJoined={setJoined} />
       ) : (
         <>
           {step === 5 ? null : (
