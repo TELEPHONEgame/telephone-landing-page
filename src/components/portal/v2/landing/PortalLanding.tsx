91cb6e712bc5a743abb7f2afde7feb4c6e120ea3
matt-diehl <mdiehl1@umbc.edu>
2024-10-18 23:38:05 -0400

    Make a PortalLink component that maintains query params

--- src/components/portal/v2/landing/PortalLanding.tsx
+++ src/components/portal/v2/landing/PortalLanding.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 import { Artist } from "../../types";
-import { Link } from "react-router-dom";
+import { PortalLink } from "../common/PortalLink";
 
 const PortalLanding = ({artist}: {artist: Artist}) => {
   return (
     <div>
       LANDING
 
-      <div><Link to="/portal/prompt">Prompt</Link></div>
-      <div><Link to="/portal/upload">Upload</Link></div>
+      <div><PortalLink to="/portal/prompt">Prompt</PortalLink></div>
+      <div><PortalLink to="/portal/upload">Upload</PortalLink></div>
     </div>
   );
 };
