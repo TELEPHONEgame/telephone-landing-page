dc1dade1f841e7434b790e3b43f160b08dcb5865
matt-diehl <mdiehl1@umbc.edu>
2024-10-21 01:12:50 -0400

    slightly more fleshed out submission list page

--- src/components/portal/v2/upload/submission/preview/PortalSubmissionPreview.tsx
+++ src/components/portal/v2/upload/submission/preview/PortalSubmissionPreview.tsx
@@ -16,8 +16,45 @@ const PortalSubmissionPreview = ({
 }: PortalSubmissionPreviewProps) => {
   return (
     <div className={styles.root}>
-      {showListInfo ? <span>{listIndex + 1}</span> : null}
-      {submission.file}
+      <div className={styles.header}>
+        <span className={styles.ordinal}>{listIndex + 1}</span>
+        <span className={styles.title}>Untitled</span>
+        {showListInfo ? (
+          <div className={styles.sortButtons}>
+            <button className={styles.sortButton}>Up</button>
+            <button className={styles.sortButton}>Down</button>
+          </div>
+        ) : null}
+      </div>
+      <div>
+        <FilePreview submission={submission} />
+      </div>
+      <div className={styles.footer}>
+        <button className={styles.footerButton}>Main Thumbnail</button>
+        <button className={styles.footerButton}>Edit</button>
+        <button className={styles.footerButton}>Delete</button>
+      </div>
+    </div>
+  );
+};
+
+const FilePreview = ({ submission }: { submission: Submission }) => {
+  switch (submission.type) {
+    case "audio":
+      return <audio src={submission.file} />;
+    case "image":
+      return <ImagePreview submission={submission} />;
+    case "video":
+      return <video src={submission.file} />;
+    default:
+      return submission.file;
+  }
+};
+
+const ImagePreview = ({ submission }: { submission: Submission }) => {
+  return (
+    <div>
+      <img src={submission.file} />
     </div>
   );
 };
