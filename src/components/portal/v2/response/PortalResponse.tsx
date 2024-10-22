6245a9b7a766d7a3b4fd3d7727cb7fc92b13b538
matt-diehl <mdiehl1@umbc.edu>
2024-10-21 22:53:42 -0400

    style the upload button

--- src/components/portal/v2/response/PortalResponse.tsx
+++ src/components/portal/v2/response/PortalResponse.tsx
@@ -1,18 +1,31 @@
 import React from "react";
 
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 import { Artist } from "@components/portal/v2/types";
 import PortalSectionHeader from "@components/portal/v2/common/page/header/PortalPageHeader";
 import PortalSubmissionList from "@components/portal/v2/response/submission/list/PortalSubmissionList";
 
-const PortalResponse = ({artist}: {artist: Artist}) => {
+const PortalResponse = ({ artist }: { artist: Artist }) => {
   return (
     <>
       <PortalSectionHeader showBackButton={true} title="Artwork Response" />
 
       <div className={styles.content}>
         <PortalSubmissionList artist={artist} />
-        <button>Upload a file</button>
+
+        <div className={styles.footer}>
+          <button className={styles.addButton}>
+            Upload a file
+            <svg
+              className={styles.addButtonIcon}
+              height="20"
+              viewBox="0 0 20 20"
+              width="20"
+            >
+              <path d="M8.66406 11.3346H0.664062V8.66797H8.66406V0.667969H11.3307V8.66797H19.3307V11.3346H11.3307V19.3346H8.66406V11.3346Z" />
+            </svg>
+          </button>
+        </div>
       </div>
     </>
   );
