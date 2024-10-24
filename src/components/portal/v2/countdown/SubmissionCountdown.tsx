2a4daa2442d7ba72cc5bcc5f330b9cbfc7d8df26
Ariel Lee <ariellee616@gmail.com>
2024-10-23 22:26:43 -0400

    Error callout mixin (sort of) + show error callout when time runs out

--- src/components/portal/v2/countdown/SubmissionCountdown.tsx
+++ src/components/portal/v2/countdown/SubmissionCountdown.tsx
@@ -23,15 +23,25 @@ export const SubmissionCountdown = ({artist}) => {
     return () => clearInterval(interval);
   }, [dueDateMs]);
 
-  return (
-    <div className={styles.root}>
+  const errorCallout = (
+    <div className={styles.errorCallout}>
+      <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12.0027 7.99923V11.9992M12.0027 15.9992H12.0127M10.2927 2.85923L1.82271 16.9992C1.64807 17.3017 1.55567 17.6445 1.5547 17.9937C1.55372 18.343 1.6442 18.6864 1.81713 18.9898C1.99007 19.2931 2.23943 19.546 2.54041 19.7231C2.8414 19.9002 3.18351 19.9954 3.53271 19.9992H20.4727C20.8219 19.9954 21.164 19.9002 21.465 19.7231C21.766 19.546 22.0153 19.2931 22.1883 18.9898C22.3612 18.6864 22.4517 18.343 22.4507 17.9937C22.4497 17.6445 22.3573 17.3017 22.1827 16.9992L13.7127 2.85923C13.5344 2.56533 13.2834 2.32235 12.9839 2.15371C12.6844 1.98508 12.3464 1.89648 12.0027 1.89648C11.659 1.89648 11.321 1.98508 11.0215 2.15371C10.722 2.32235 10.471 2.56533 10.2927 2.85923Z" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
+      </svg>
+      You have run out of time. Please request an extension.
+    </div>
+  );
+
+  return (<>
+    {timeLeftMs === 0 ? errorCallout : null}
+    <div className={styles.content}>
       {firstName}, your submission is due in:
       <TimeLeftDisplay timeLeftMs={timeLeftMs} />
       <button className={styles.extensionButton}>
         Request an extension
       </button>
     </div>
-  );
+  </>);
 };
 
 const TimeLeftDisplay = ({timeLeftMs}: {timeLeftMs: number}) => {
