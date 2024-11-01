ac32bcc8264143c64175357eb3d1b545916db9d0
Ariel Lee <ariellee616@gmail.com>
2024-10-31 22:43:32 -0400

    Don't show artist portal link in header if artist token isn't present

--- src/components/common/v2/header/TelephoneHeader.tsx
+++ src/components/common/v2/header/TelephoneHeader.tsx
@@ -4,13 +4,16 @@ import { PortalLink } from "../../../portal/v2/common/PortalLink";
 import styles from "./styles.module.scss"
 
 const TelephoneHeader = () => {
+  const queryParams = new URLSearchParams(window.location.search);
+  const token = queryParams.get("token");
+
   return (
     <div className={styles.root}>
       <PortalLink to="/" className={styles.logo}>
         TELEPHONE
       </PortalLink>
       <div>
-        <PortalLink to="/portal">Artist Portal</PortalLink>
+        {token ? <PortalLink to="/portal">Artist Portal</PortalLink> : null}
       </div>
     </div>
   );
