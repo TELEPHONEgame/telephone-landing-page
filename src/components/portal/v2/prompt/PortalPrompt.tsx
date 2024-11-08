fbcc088e198730c8615704f579735faec8e93ff2
Ariel Lee <ariellee616@gmail.com>
2024-11-07 22:04:51 -0500

    Lay out some styled UI elements for artist prompt page

--- src/components/portal/v2/prompt/PortalPrompt.tsx
+++ src/components/portal/v2/prompt/PortalPrompt.tsx
@@ -1,16 +1,46 @@
 import React from "react";
 
+import styles from "./styles.module.scss";
 import { useArtist } from "@components/portal/v2/Portal";
+import PortalAccordion from "@components/portal/v2/common/accordion/PortalAccordion";
 import PortalSectionHeader from "@components/portal/v2/common/page/header/PortalPageHeader";
 
 const PortalPrompt = () => {
   const {artist} = useArtist();
 
   return (
-    <div>
-      <PortalSectionHeader showBackButton={true} title="Artwork Response" />
-      PROMPT
-    </div>
+    <>
+      <PortalSectionHeader showBackButton={true} title="Artistic Prompt" />
+      <div className={styles.content}>
+        Here is the secret message being whispered to you.
+        Your job is to create an art work to be whispered to the next player.
+
+        <div className={styles.accordion}>
+          <PortalAccordion title="Instructions">
+            Lorem ipsum odor amet, consectetuer adipiscing elit.
+            Maecenas curabitur ullamcorper habitasse nulla dapibus netus ultrices maximus.
+            Metus suspendisse elit magnis; consectetur magna est egestas penatibus id.
+            Amolestie nullam a fringilla ligula id eget placerat. Auctor conubia dictum
+            posuere ac proin dictum mollis malesuada orci. Laoreet justo tristique diam dis
+            odio sem consectetur ridiculus. Porta consequat suscipit placerat conubia proin
+            at platea. Ex fringilla nibh ullamcorper volutpat amet nibh eros torquent.
+          </PortalAccordion>
+          <PortalAccordion title="Your assignment">
+            Lorem ipsum odor amet, consectetuer adipiscing elit.
+            Maecenas curabitur ullamcorper habitasse nulla dapibus netus ultrices maximus.
+            Metus suspendisse elit magnis; consectetur magna est egestas penatibus id.
+            Amolestie nullam a fringilla ligula id eget placerat. Auctor conubia dictum
+            posuere ac proin dictum mollis malesuada orci. Laoreet justo tristique diam dis
+            odio sem consectetur ridiculus. Porta consequat suscipit placerat conubia proin
+            at platea. Ex fringilla nibh ullamcorper volutpat amet nibh eros torquent.
+          </PortalAccordion>
+        </div>
+
+        <button className={styles.downloadButton}>
+          Download your assignment
+        </button>
+      </div>
+    </>
   );
 };
 
