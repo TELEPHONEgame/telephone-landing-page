97985ce8531599e9e19e4157b70dc2373ee2dd38
matt-diehl <mdiehl1@umbc.edu>
2024-11-17 18:16:35 -0500

    Fix download to actually download the parent artwork

--- src/components/portal/v2/prompt/PortalPrompt.tsx
+++ src/components/portal/v2/prompt/PortalPrompt.tsx
@@ -8,42 +8,46 @@ import PortalSectionHeader from "@components/portal/v2/common/page/header/Portal
 import PortalPromptList from "@components/portal/v2/prompt/list/PortalPromptList";
 
 const PortalPrompt = () => {
-  const {artist} = useArtist();
+  const { artist } = useArtist();
 
   return (
     <>
       <PortalSectionHeader showBackButton={true} title="Artistic Prompt" />
       <div className={styles.content}>
-        Here is the secret message being whispered to you.
-        Your job is to create an art work to be whispered to the next player.
-
+        Here is the secret message being whispered to you. Your job is to create
+        an art work to be whispered to the next player.
         <div className={styles.accordion}>
           <PortalAccordion title="Instructions">
-            Lorem ipsum odor amet, consectetuer adipiscing elit.
-            Maecenas curabitur ullamcorper habitasse nulla dapibus netus ultrices maximus.
-            Metus suspendisse elit magnis; consectetur magna est egestas penatibus id.
-            Amolestie nullam a fringilla ligula id eget placerat. Auctor conubia dictum
-            posuere ac proin dictum mollis malesuada orci. Laoreet justo tristique diam dis
-            odio sem consectetur ridiculus. Porta consequat suscipit placerat conubia proin
-            at platea. Ex fringilla nibh ullamcorper volutpat amet nibh eros torquent.
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Maecenas
+            curabitur ullamcorper habitasse nulla dapibus netus ultrices
+            maximus. Metus suspendisse elit magnis; consectetur magna est
+            egestas penatibus id. Amolestie nullam a fringilla ligula id eget
+            placerat. Auctor conubia dictum posuere ac proin dictum mollis
+            malesuada orci. Laoreet justo tristique diam dis odio sem
+            consectetur ridiculus. Porta consequat suscipit placerat conubia
+            proin at platea. Ex fringilla nibh ullamcorper volutpat amet nibh
+            eros torquent.
           </PortalAccordion>
           <PortalAccordion title="Your assignment">
-            Lorem ipsum odor amet, consectetuer adipiscing elit.
-            Maecenas curabitur ullamcorper habitasse nulla dapibus netus ultrices maximus.
-            Metus suspendisse elit magnis; consectetur magna est egestas penatibus id.
-            Amolestie nullam a fringilla ligula id eget placerat. Auctor conubia dictum
-            posuere ac proin dictum mollis malesuada orci. Laoreet justo tristique diam dis
-            odio sem consectetur ridiculus. Porta consequat suscipit placerat conubia proin
-            at platea. Ex fringilla nibh ullamcorper volutpat amet nibh eros torquent.
-
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Maecenas
+            curabitur ullamcorper habitasse nulla dapibus netus ultrices
+            maximus. Metus suspendisse elit magnis; consectetur magna est
+            egestas penatibus id. Amolestie nullam a fringilla ligula id eget
+            placerat. Auctor conubia dictum posuere ac proin dictum mollis
+            malesuada orci. Laoreet justo tristique diam dis odio sem
+            consectetur ridiculus. Porta consequat suscipit placerat conubia
+            proin at platea. Ex fringilla nibh ullamcorper volutpat amet nibh
+            eros torquent.
             <div className={styles.promptList}>
               <PortalPromptList artist={artist} />
             </div>
           </PortalAccordion>
         </div>
-
         <div className={styles.footer}>
-          <button className={styles.downloadButton} onClick={() => api.downloadAssignment(artist.submissions)}>
+          <button
+            className={styles.downloadButton}
+            onClick={() => api.downloadAssignment(artist.parent.submissions)}
+          >
             Download your assignment
           </button>
         </div>
