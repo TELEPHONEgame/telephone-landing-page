c27ded4343cd512921af76e94f3bb4deb6442daa
matt-diehl <mdiehl1@umbc.edu>
2024-11-17 19:59:44 -0500

    Disable submit button on landing after submission

--- src/components/portal/v2/landing/PortalLanding.tsx
+++ src/components/portal/v2/landing/PortalLanding.tsx
@@ -8,12 +8,12 @@ import styles from "./styles.module.scss";
 import PortalSectionHeader from "@components/portal/v2/common/page/header/PortalPageHeader";
 
 const PortalLanding = () => {
-  const {artist, reloadArtist} = useArtist();
+  const { artist, reloadArtist } = useArtist();
 
   const artistHasUploads = artist.submissions.length > 0;
   const dueDateMs = new Date(artist.due).getTime();
 
-  const [timeLeftMs, setTimeLeftMs] = useState<number|null>(null);
+  const [timeLeftMs, setTimeLeftMs] = useState<number | null>(null);
   const timesUp = timeLeftMs <= 0;
 
   useEffect(() => {
@@ -31,17 +31,27 @@ const PortalLanding = () => {
   }, [dueDateMs]);
 
   const submit = () => {
-    updateArtist(
-      artist.id, 
-      {submitted: new Date().toISOString()}
-    ).then(() => reloadArtist());
+    updateArtist(artist.id, { submitted: new Date().toISOString() }).then(() =>
+      reloadArtist()
+    );
   };
 
   const submitSuccessCallout = (
     <div className={styles.successCallout}>
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-        <rect x="0.75" y="0.75" width="22.5" height="22.5" rx="11.25" stroke="white" strokeWidth="1.5"/>
-        <path d="M8.55353 15.9464L5.49877 12.8916C5.33429 12.7268 5.111 12.6341 4.87813 12.6341C4.64527 12.6341 4.42197 12.7268 4.2575 12.8916C3.91417 13.235 3.91417 13.7896 4.2575 14.1329L7.93729 17.8127C8.28063 18.156 8.83524 18.156 9.17857 17.8127L18.4925 8.49876C18.8358 8.15543 18.8358 7.60082 18.4925 7.25749C18.328 7.09264 18.1047 7 17.8719 7C17.639 7 17.4157 7.09264 17.2512 7.25749L8.55353 15.9464Z" fill="white"/>
+        <rect
+          x="0.75"
+          y="0.75"
+          width="22.5"
+          height="22.5"
+          rx="11.25"
+          stroke="white"
+          strokeWidth="1.5"
+        />
+        <path
+          d="M8.55353 15.9464L5.49877 12.8916C5.33429 12.7268 5.111 12.6341 4.87813 12.6341C4.64527 12.6341 4.42197 12.7268 4.2575 12.8916C3.91417 13.235 3.91417 13.7896 4.2575 14.1329L7.93729 17.8127C8.28063 18.156 8.83524 18.156 9.17857 17.8127L18.4925 8.49876C18.8358 8.15543 18.8358 7.60082 18.4925 7.25749C18.328 7.09264 18.1047 7 17.8719 7C17.639 7 17.4157 7.09264 17.2512 7.25749L8.55353 15.9464Z"
+          fill="white"
+        />
       </svg>
       Success, you have submitted your artwork
     </div>
@@ -50,15 +60,19 @@ const PortalLanding = () => {
   return (
     <div className={styles.root}>
       <PortalSectionHeader title="Artist Portal" />
-      {artist.submitted ?
-        submitSuccessCallout :
-        <SubmissionCountdown firstName={artist.first_name} timeLeftMs={timeLeftMs} /> 
-      }
+      {artist.submitted ? (
+        submitSuccessCallout
+      ) : (
+        <SubmissionCountdown
+          firstName={artist.first_name}
+          timeLeftMs={timeLeftMs}
+        />
+      )}
       <h2>Tasks</h2>
       <div className={styles.tasksSubtitle}>
         There are 2 things that we need from you.
       </div>
-      <TaskButton 
+      <TaskButton
         isDisabled={timesUp}
         label="View your artistic prompt"
         route="/portal/prompt"
@@ -72,7 +86,7 @@ const PortalLanding = () => {
       />
       <button
         className={styles.submitButton}
-        disabled={timesUp}
+        disabled={timesUp || !!artist.submitted}
         onClick={submit}
       >
         Submit
@@ -85,53 +99,70 @@ function calculateTimeLeftMs(dueDateMs: number) {
   const timeLeftMs = dueDateMs - new Date().getTime();
   // Don't let the time left go negative.
   return Math.max(timeLeftMs, 0);
-};
+}
 
 const TaskButton = ({
-  doneMessage = '',
+  doneMessage = "",
   isDisabled = false,
   isDone = false,
   label,
   route,
 }: {
-  doneMessage?: string,
+  doneMessage?: string;
   isDisabled?: boolean;
-  isDone?: boolean,
-  label: string,
-  route: string,
+  isDone?: boolean;
+  label: string;
+  route: string;
 }) => {
-  const taskButtonLabel =
+  const taskButtonLabel = (
     <>
       {label}
       <svg width="18" height="16" viewBox="0 0 18 16">
-        <path d="M2 8L16 8M16 8L10 2M16 8L10 14" stroke="black" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+        <path
+          d="M2 8L16 8M16 8L10 2M16 8L10 14"
+          stroke="black"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
       </svg>
     </>
+  );
 
-  const taskDoneIndicator = 
+  const taskDoneIndicator = (
     <div className={styles.taskDoneIndicator}>
       <div className={styles.doneIcon}>
-        <svg width="12" height="10" viewBox="0 0 12 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M3.8624 7.58358L1.54906 5.27025C1.42451 5.14542 1.25541 5.07526 1.07906 5.07526C0.902717 5.07526 0.733617 5.14542 0.609062 5.27025C0.349063 5.53025 0.349063 5.95025 0.609062 6.21025L3.39573 8.99692C3.65573 9.25692 4.07573 9.25692 4.33573 8.99692L11.3891 1.94358C11.6491 1.68358 11.6491 1.26358 11.3891 1.00358C11.2645 0.878749 11.0954 0.808594 10.9191 0.808594C10.7427 0.808594 10.5736 0.878749 10.4491 1.00358L3.8624 7.58358Z" fill="black"/>
+        <svg
+          width="12"
+          height="10"
+          viewBox="0 0 12 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M3.8624 7.58358L1.54906 5.27025C1.42451 5.14542 1.25541 5.07526 1.07906 5.07526C0.902717 5.07526 0.733617 5.14542 0.609062 5.27025C0.349063 5.53025 0.349063 5.95025 0.609062 6.21025L3.39573 8.99692C3.65573 9.25692 4.07573 9.25692 4.33573 8.99692L11.3891 1.94358C11.6491 1.68358 11.6491 1.26358 11.3891 1.00358C11.2645 0.878749 11.0954 0.808594 10.9191 0.808594C10.7427 0.808594 10.5736 0.878749 10.4491 1.00358L3.8624 7.58358Z"
+            fill="black"
+          />
         </svg>
       </div>
       {doneMessage}
     </div>
+  );
 
-  const task = isDisabled ?
+  const task = isDisabled ? (
     <button disabled className={styles.taskButton}>
       {taskButtonLabel}
-    </button> :
+    </button>
+  ) : (
     <>
       <PortalLink to={route} className={styles.taskButton}>
         {taskButtonLabel}
       </PortalLink>
       {isDone ? taskDoneIndicator : null}
     </>
-
-  return (
-    <div className={styles.taskContainer}>{task}</div>
   );
+
+  return <div className={styles.taskContainer}>{task}</div>;
 };
 
 export default PortalLanding;
