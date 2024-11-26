af2e0a48112a6e539a9f5e9a5911423633c73905
matt-diehl <mdiehl1@umbc.edu>
2024-11-25 22:02:41 -0500

    Add written work instructions note

--- src/components/portal/v2/response/submission/edit/PortalSubmissionEdit.tsx
+++ src/components/portal/v2/response/submission/edit/PortalSubmissionEdit.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import styles from "./styles.module.scss";
 import * as api from "@components/portal/v2/api";
@@ -10,9 +10,8 @@ import PortalSectionHeader from "@components/portal/v2/common/page/header/Portal
 import PortalAccordion from "@components/portal/v2/common/accordion/PortalAccordion";
 import PortalSubmissionEditForm from "@components/portal/v2/response/submission/edit/form/PortalSubmissionEditForm";
 
-
 const PortalSubmissionEdit = () => {
-  const {artist} = useArtist();
+  const { artist } = useArtist();
   const { submissionId } = useParams();
   const { search } = useLocation();
   const { reloadArtist } = useArtist();
@@ -68,17 +67,40 @@ const PortalSubmissionEdit = () => {
 
       <div className={styles.accordion}>
         <PortalAccordion title="How do you want your artwork presented?">
-          Now that you've imported your artwork, you have the option to provide
-          additional details. Please take a moment to review the{" "}
-          <a href="/faq" target="_blank">
-            Usage and Permissions guidelines
-          </a>{" "}
-          related to your artwork to ensure you understand how it may be used
-          and shared.
+          <p>
+            Now that you've imported your artwork, you have the option to
+            provide additional details. Please take a moment to review the{" "}
+            <a href="/faq" target="_blank">
+              Usage and Permissions guidelines
+            </a>{" "}
+            related to your artwork to ensure you understand how it may be used
+            and shared.
+          </p>
+
+          <p>
+            <strong>A note on written works:</strong>
+          </p>
+          <p>
+            We want your text art to be displayed the way you want it. If you
+            hit Enter, it will make a new paragraph. If you hit Shift + Enter,
+            it will create a line break. If you click the "Use poem formatting"
+            checkbox, we will preserve your line breaks, even if your lines are
+            very long like a Walt Whitman poem (at which point, readers will
+            scroll horizontally to see all of your words).
+          </p>
+          <p>
+            If you have any questions or concerns, please contact us as
+            telephone.outreach@gmail.com and we will try to display your work
+            the way that you intended.
+          </p>
         </PortalAccordion>
       </div>
 
-      <PortalSubmissionEditForm submission={submission} onSubmit={onSubmit} onDelete={onDelete} />
+      <PortalSubmissionEditForm
+        submission={submission}
+        onSubmit={onSubmit}
+        onDelete={onDelete}
+      />
 
       <LoadingOverlay isLoading={isSaving} message="Saving..." />
       <LoadingOverlay isLoading={isDeleting} message="Deleting..." />
